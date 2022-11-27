719647ba3d1a6815de7c863970e7e32f12d70bb7
Bing Liu <sipingal@163.com>
2022-11-27 12:01:28 +0800

    update openmpi's mpi settings

--- scripts/mpi_settings.sh
+++ scripts/mpi_settings.sh
@@ -13,30 +13,55 @@ hpc_enable_efa()
     #export OMPI_MCA_pml=ob1
     #export OMPI_MCA_btl=tcp,vader,self
     #else
-    #export OMPI_MCA_pml=ob1 export
-    #OMPI_MCA_btl=tcp,vader,self
+    #export OMPI_MCA_pml=ob1
+    #export OMPI_MCA_btl=tcp,vader,self
     #fi
 
     case ${HPC_MPI} in
 	"intelmpi")
-	    export I_MPI_OFI_PROVIDER=efa
 	    export I_MPI_FABRICS=ofi
+	    export I_MPI_OFI_PROVIDER=efa
 	    #export I_MPI_FABRICS="shm:ofi"
 	    ###export FI_PROVIDER=efa
 	    ;;
 	"openmpi")
 	    # more details see: ompi_info
-	    export OMPI_MCA_pml=cm
 	    export OMPI_MCA_mtl=ofi
+	    export OMPI_MCA_pml=cm
+	    export OMPI_MCA_mtl_ofi_provider_include=efa
 	    ;;
 	"mpich")
-	    continue
+	    FI_PROVIDER=efa
 	    ;;
 	"mvapich")
-	    continue
+	    FI_PROVIDER=efa
 	    ;;
 	*)
+	    FI_PROVIDER=efa
+    esac
+}
+
+hpc_set_mpi()
+{
+    case ${HPC_MPI} in
+	"intelmpi")
 	    continue
+	    #export I_MPI_FABRICS=ofi
+	    #export I_MPI_OFI_PROVIDER=tcp
+	    ;;
+	"openmpi")
+	    # more details see: ompi_info
+	    export OMPI_MCA_btl=tcp,vader,self
+	    export OMPI_MCA_pml=ob1
+	    ;;
+	"mpich")
+	    FI_PROVIDER=udp
+	    ;;
+	"mvapich")
+	    FI_PROVIDER=udp
+	    ;;
+	*)
+	    FI_PROVIDER=sockets
     esac
 }
 
@@ -60,11 +85,9 @@ hpc_enable_mpi_debug()
 	"mpich")
 	    # more details, see README.envvar
 	    export MPIR_CVAR_DEBUG_SUMMARY=1
-	    continue
 	    ;;
 	"mvapich")
 	    export MV2_SHOW_CPU_BINDING=1
-	    continue
 	    ;;
 	*)
 	    continue
@@ -75,6 +98,8 @@ hpc_enable_mpi_debug()
 if (fi_info -p efa > /dev/null 2>&1)
 then
     hpc_enable_efa
+else
+    hpc_set_mpi
 fi
 
 if [ ${HPC_MPI_DEBUG} -eq 1 ]
