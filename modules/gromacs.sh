5af8c01dadd9ed10f7864196ad78517e4415c640
Bing Liu <sipingal@163.com>
2022-12-06 17:08:46 +0800

    add new module gromacs

---
+++ modules/gromacs.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 by Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+
+GROMACS_VERSION=${2:-2022.4}
+GROMACS_SRC=gromacs-${GROMACS_VERSION}.tar.gz
+DISABLE_COMPILER_ENV=false
+
+install_sys_dependency_for_gromacs()
+{
+    return
+}
+
+download_gromacs()
+{
+    echo "zzz *** $(date) *** Downloading source code ${GROMACS_SRC}"
+    if [ -f ${GROMACS_SRC} ]
+    then
+        return
+    else
+         wget https://ftp.gromacs.org/gromacs/${GROMACS_SRC}
+         return $?
+    fi
+}
+
+install_gromacs()
+{
+    rm -rf ${GROMACS_SRC%.tar.gz}
+    tar xf ${GROMACS_SRC}
+    cd ${GROMACS_SRC%.tar.gz}
+    if [ -f ../../patch/gromacs/gromacs-${GROMACS_VERSION}-${HPC_COMPILER}-${HPC_MPI}.patch ]
+    then
+	patch -Np1 < ../../patch/gromacs/gromacs-${GROMACS_VERSION}-${HPC_COMPILER}-${HPC_MPI}.patch
+    fi
+    mkdir -p build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX=${HPC_PREFIX}/${HPC_COMPILER}/${HPC_MPI} -DGMX_BUILD_OWN_FFTW=ON -DGMX_MPI=ON -DGMX_MP=ON
+    make && sudo --preserve-env=PATH,LD_LIBRARY_PATH env make install && cd ../..
+}
+
+update_gromacs_version()
+{
+    MODULE_VERSION=${GROMACS_VERSION}
+}
