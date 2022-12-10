fa015fe71990f99373b7ed86fede3ecbf5d5349e
Bing Liu <sipingal@163.com>
2022-12-10 16:40:42 +0800

    add new module Intel(R) MPI Benchmarks(IMB)

---
+++ modules/imb.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 by Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+
+IMB_VERSION=${2:-2021.3}
+IMB_SRC=IMB-v${IMB_VERSION}.tar.gz
+DISABLE_COMPILER_ENV=false
+
+install_sys_dependency_for_imb()
+{
+    return
+}
+
+download_imb()
+{
+    echo "zzz *** $(date) *** Downloading source code ${IMB_SRC}"
+    if [ -f ${IMB_SRC} ]
+    then
+        return
+    else
+         wget https://github.com/intel/mpi-benchmarks/archive/refs/tags/${IMB_SRC}
+         return $?
+    fi
+}
+
+install_imb()
+{
+    rm -rf ${IMB_SRC%.tar.gz}
+    tar xf ${IMB_SRC}
+    cd ${IMB_SRC%.tar.gz}
+    if [ -f ../../patch/imb/imb-${IMB_VERSION}-return-value.patch ]
+    then
+	patch -Np1 < ../../patch/imb/imb-${IMB_VERSION}-return-value.patch
+    fi
+    ./configure CC=mpicc CXX=mpicxx
+    make && cd .. && sudo mv "${IMB_SRC%.tar.gz}" "${HPC_PREFIX}/${HPC_COMPILER}/${HPC_MPI}/"
+}
+
+update_imb_version()
+{
+    MODULE_VERSION=${IMB_VERSION}
+}
